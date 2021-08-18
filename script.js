1c4cffa7eb461d077eb4a8959d1d54ac99ac7e70
varinmammoth <varinmammoth@gmail.com>
2021-08-18 15:03:06 +0700

    Encountered TypeError line 18

--- script.js
+++ script.js
@@ -21,19 +21,19 @@ function versus(humanSelection, computerSelection) {
     console.log('You chose: ' + humanSelection + '. Computer chose: ' + computerSelection + '.');
     if (humanSelection === computerSelection) {
         humanOutcome = 'tie'
-        console.log('You tied.')
+        console.log('You tied the round.')
         return humanOutcome;
     }
 
     if (humanSelection === 'ROCK') {
         if (computerSelection ==='SCISSOR') {
             humanOutcome = 'win';
-            console.log(humanSelection + " beats " + computerSelection + ". You won!")
+            console.log(humanSelection + " beats " + computerSelection + ". You won the round!")
             return humanOutcome;
         }
         else {
             humanOutcome = 'loss';
-            console.log(computerSelection + " beats " + humanSelection + ". You lost!")
+            console.log(computerSelection + " beats " + humanSelection + ". You lost the round!")
             return humanOutcome
         }
     }
@@ -41,12 +41,12 @@ function versus(humanSelection, computerSelection) {
     if (humanSelection === 'SCISSOR') {
         if (computerSelection ==='PAPER') {
             humanOutcome = 'win';
-            console.log(humanSelection + " beats " + computerSelection + ". You won!")
+            console.log(humanSelection + " beats " + computerSelection + ". You won the round!")
             return humanOutcome;
         }
         else {
             humanOutcome = 'loss';
-            console.log(computerSelection + " beats " + humanSelection + ". You lost!")
+            console.log(computerSelection + " beats " + humanSelection + ". You lost the round!")
             return humanOutcome
         }
     }
@@ -54,13 +54,47 @@ function versus(humanSelection, computerSelection) {
     if (humanSelection === 'PAPER') {
         if (computerSelection ==='ROCK') {
             humanOutcome = 'win';
-            console.log(humanSelection + " beats " + computerSelection + ". You won!")
+            console.log(humanSelection + " beats " + computerSelection + ". You won the round!")
             return humanOutcome;
         }
         else {
             humanOutcome = 'loss';
-            console.log(computerSelection + " beats " + humanSelection + ". You lost!")
+            console.log(computerSelection + " beats " + humanSelection + ". You lost the round!")
             return humanOutcome
         }
     }
+}
+
+function game() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let humanSelection;
+    let computerSelection;
+    while (humanScore <= 3 || computerScore <=3) {
+        humanSelection = prompt('Enter your move (rock/paper/scissor): ');
+        computerSelection = computerPlay;
+        humanOutcome = versus(humanSelection, computerSelection);
+        if (humanOutcome === 'win') {
+            humanScore += 1;
+            console.log('Score: Human: ' + humanScore + " Computer: " + computerScore);
+        }
+        else if (humanOutcome === 'loss') {
+            computerScore += 1;
+            console.log('Score: Human: ' + humanScore + " Computer: " + computerScore);
+        }
+    
+    if (humanScore === 3) {
+        console.log("You won the game with a score of " + humanScore + " to " + computerScore);
+    }
+    else {
+        console.log("You loss the game with a score of " + humanScore + " to " + computerScore);
+    }
+    }
+
+
+
+
+
+
+
 }
